4a7c1366307c323a600af9a3cd6f077dde55a452
MaherHazbaz <maherhazbaz7@gmail.com>
2024-07-25 17:01:50 +0530

    routes over

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Index from "./pages/Index";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Route, Router, Routes } from "react-router-dom";
+
+const NotFound = () => {
+  return <>hi</>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About.jsx />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
